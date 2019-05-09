d89022f3314234113b0c91886e125a6da78b7a6f
Tristan Reed <mail@trisreed.com>
2019-05-09 09:08:50 +0800

    Elaborate the HVS analyser file and remove the limitations of the first two items (seems to cause issues, however).

--- hvsnetwork.py
+++ hvsnetwork.py
@@ -3,7 +3,9 @@
 HVS Network Mapper
 Planning and Transport Research Centre (PATREC)
 
-This script is used to map the HVS Network data to OSM components.
+This script is used to map the HVS Network data to OSM node components. It is
+currently a work-in-progress as part of investigating methods to match other
+spatial data to OSM components.
 """
 
 
@@ -12,7 +14,8 @@ __version__ = "0.1.0"
 
 
 """ Specify the URL for the HVS network. """
-HVS_SOURCE = "https://opendata.arcgis.com/datasets/9eddd767132d46aebdef328ec79573d3_46.geojson"
+HVS_SOURCE = "https://opendata.arcgis.com/datasets/" +\
+    "9eddd767132d46aebdef328ec79573d3_46.geojson"
 
 
 """ Specify the base URL for the OSRM instance. """
@@ -20,17 +23,28 @@ OSRM_SOURCE = "https://router.project-osrm.org/"
 
 
 """ Import required libraries. """
-import pandas, requests, tqdm
+import pandas, requests, time, tqdm
 
 
 def main():
 
-    """ Pull the GeoJSON file from Main Roads / ESRI. Only care about the 
-    'feature list' so pull that out straight away. """
+    """ Pull the GeoJSON file from Main Roads / ESRI. """
     print("Retrieving data from Main Roads / ESRI...")
     hvs_data = requests.get(HVS_SOURCE)
-    """ @note For testing I am limiting this to the first two feature(s). """
-    hvs_json = hvs_data.json()["features"][0:2]
+    hvs_json = hvs_data.json()
+
+    """ Check for a CRS key and alert either way. """
+    if ("crs" in hvs_json):
+        print("""NOTE: A CRS has been specified. Ensure it is EPSG:4326 (WGS84)
+        before relying on the output of this script.""")
+    else:
+        print("""NOTE: There is no CRS specified in the root element. If the
+        file is standards compliant, this means it is in EPSG:4326 (WGS84).
+        However, this might not be the case, or it may be incorrectly set on
+        each feature.""")
+
+    """ Only care about the 'feature list' so pull that out straight away. """
+    hvs_json = hvs_json["features"]
 
     """ Create a list for dictionaries for output. """
     return_list = []
@@ -54,22 +68,27 @@ def main():
         osrm_url = OSRM_SOURCE + "route/v1/driving/" + road_geography + \
             "?steps=false&geometries=polyline&overview=full&annotations=true"
 
+        """ Specify which match from OSRM to take (this is mainly here to
+        remind me to check it and work out which match is best). """
+        chosen_osrm_match = 0
+
         """ Get the data from OSRM. 'Routes' is the key of interest. """
-        """ @note There will be multiple matches. Take the first, for now. """
         osrm_data = requests.get(osrm_url)
         osrm_json = osrm_data.json()
         osrm_node_dualist = [leg["annotation"]["nodes"] for leg in \
-            osrm_json["routes"][0]["legs"]]
+            osrm_json["routes"][chosen_osrm_match]["legs"]]
         
         """ Put it all together and add to list. """
         return_list.append({"id": road_id, "name": road_name, 
             "node_list": osrm_node_dualist})
 
-        """ @note the OSRM online service is globally limited to 5999 requests 
+        """ The OSRM online service is globally limited to 5999 requests 
         a minute. Generally, network latency means we aren't hitting it too 
-        much. But, be aware! """
+        much. But, to be safe, we will sleep for half a second. """
+        time.sleep(0.5)
     
-    """ Lazy mode: use Pandas to output to CSV. """
+    """ Lazy mode: use Pandas to output to convert to a DataFrame to output to
+    the CSV format. """
     print("Outputting to CSV...")
     output_df = pandas.DataFrame(return_list)
     output_df.to_csv("output.csv", index = False)
