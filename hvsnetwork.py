15e34bdde1d82507941dcd88ff73b6e404334027
Tristan Reed <mail@trisreed.com>
2019-05-09 12:45:27 +0800

    Some more adjustments to it working and adding features.

--- hvsnetwork.py
+++ hvsnetwork.py
@@ -23,7 +23,7 @@ OSRM_SOURCE = "https://router.project-osrm.org/"
 
 
 """ Import required libraries. """
-import pandas, requests, time, tqdm
+import pandas, polyline, requests, time, tqdm
 
 
 def main():
@@ -43,8 +43,9 @@ def main():
         However, this might not be the case, or it may be incorrectly set on
         each feature.""")
 
-    """ Only care about the 'feature list' so pull that out straight away. """
-    hvs_json = hvs_json["features"]
+    """ Only care about the 'feature list' so pull that out straight away. If
+    you wish to take a subset, specify within the second brackets. """
+    hvs_json = hvs_json["features"][0:100]
 
     """ Create a list for dictionaries for output. """
     return_list = []
@@ -59,14 +60,14 @@ def main():
         """ Pull out the geographic data for this road. """
         road_geography = each_feature["geometry"]["coordinates"]
 
-        """ Convert that into a String of the correct format. """
-        """ @todo ensure both OSRM and the RVS are in same CRS. """
-        road_geography = ";".join([",".join(str(x) for x in coord) for coord \
-            in road_geography])
+        """ Convert that into a list of the correct format. """
+        road_geography = polyline.encode([(coord[0], coord[1]) for coord in \
+            road_geography])
+        print(road_geography)
 
         """ Generate the URL for the OSRM Matcher. """
         osrm_url = OSRM_SOURCE + "route/v1/driving/" + road_geography + \
-            "?steps=false&geometries=polyline&overview=full&annotations=true"
+            "?steps=false&geometries=geojson&overview=full&annotations=true"
 
         """ Specify which match from OSRM to take (this is mainly here to
         remind me to check it and work out which match is best). """
@@ -74,33 +75,37 @@ def main():
 
         """ Get the data from OSRM. 'Routes' is the key of interest. """
         osrm_data = requests.get(osrm_url)
-        print(osrm_data)
+
+        """ Store a value to use to calculate backoff timing. """
+        backoff_timing = 0.25
 
         """ Sometimes (I am guessing due to the rate limiting) the service will
         return nothing (well, a 429). If so, go back and do it again. """
         while (osrm_data.status_code != 200):
-            time.sleep(0.5)
+            print(osrm_data.status_code)
+            print(osrm_data.headers)
+            time.sleep(backoff_timing)
+            backoff_timing = backoff_timing + (1.0 * backoff_timing)
             osrm_data = requests.get(osrm_url)
         
         """ Continue on, processing the result. """
         osrm_json = osrm_data.json()
         osrm_node_dualist = [leg["annotation"]["nodes"] for leg in \
             osrm_json["routes"][chosen_osrm_match]["legs"]]
+
+        """ Pull out the list of coordinates as well. """
+        osrm_node_coordlist = osrm_json["routes"][chosen_osrm_match]\
+            ["geometry"]["coordinates"]
         
         """ Put it all together and add to list. """
         return_list.append({"id": road_id, "name": road_name, 
-            "node_list": osrm_node_dualist})
-
-        """ The OSRM online service is globally limited to 5999 requests 
-        a minute. Generally, network latency means we aren't hitting it too 
-        much. But, to be safe, we will sleep for half a second. """
-        # time.sleep(0.5)
+            "node_list": osrm_node_dualist, "coord_list": osrm_node_coordlist})
     
     """ Lazy mode: use Pandas to output to convert to a DataFrame to output to
     the CSV format. """
     print("Outputting to CSV...")
     output_df = pandas.DataFrame(return_list)
-    output_df.to_csv("output.csv", index = False)
+    output_df.to_csv("hvsnetwork.csv", index = False)
 
 
 if __name__ == "__main__":
